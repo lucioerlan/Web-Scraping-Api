13198ecdc83c617a1ce16dc4f57c3d0749db490e
luciorox11@outlook.com <luciodevinfo@gmail.com>
2021-03-30 10:07:23 -0300

    :art: chore format

--- src/utils/webConfig.js
+++ src/utils/webConfig.js
@@ -1,5 +1,4 @@
 module.exports = {
-
   urlSite:
     'https://www.vagas.com.br/vagas-de-analista-de-seguranca-da-informacao?c%5B%5D=Belo+Horizonte&c%5B%5D=Contagem&c%5B%5D=Betim&ordenar_por=mais_recentes',
 
