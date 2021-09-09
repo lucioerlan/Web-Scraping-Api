57c44091f34cb5fbf234c035117eac0a24abf27b
lucioerlan <luciodevinfo@gmail.com>
2021-09-09 02:11:33 -0300

    chore remove files

--- src/routes/scraping-routes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const ScrapingController = require('../controllers/scraping-controller');
-
-const scrapingController = new ScrapingController();
-
-router.get('/jobs', scrapingController.show);
-
-module.exports = router;
